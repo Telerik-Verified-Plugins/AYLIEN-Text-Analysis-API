9872360b9fd7e01f32920af88d7331356e79ae13
Mehfuz Hossain <mehfuz@gmail.com>
2014-09-03 17:46:33 -0700

    inlcuded sentiment

--- www/aylien.js
+++ www/aylien.js
@@ -13,6 +13,10 @@ Aylien.prototype.hashtags = function(arg, successCallback){
     exec(successCallback,null, "Aylien", "hashtags", [encodeURIComponent(arg)]);
 }
 
+Aylien.prototype.sentiment = function(arg, successCallback){
+		exec(successCallback,null, "Aylien", "sentiment", [encodeURIComponent(arg)]);
+}
+
 if (typeof module != 'undefined' && module.exports) {
   module.exports = new Aylien();
 }
